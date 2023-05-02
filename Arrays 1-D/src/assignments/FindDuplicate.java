b58855d7e2aea8cfccbce301d679a8fc6dbfa8ec
shivanirathore24 <shivani.rathore2409@gmail.com>
2023-05-02 12:40:48 +0530

    Added Arrays 1-D

--- Arrays 1-D/src/assignments/FindDuplicate.java
+++ Arrays 1-D/src/assignments/FindDuplicate.java
@@ -9,7 +9,7 @@ public class FindDuplicate {
 	public static int findDuplicate(int[] arr)
 	{
 		int n = arr.length;
-		for(int i =0; i< n-1; i++)
+		for(int i =0; i< n-1; i++)  // O(n^2)
 		{
 			for(int j = i+1; j<n ; j++)
 			{
