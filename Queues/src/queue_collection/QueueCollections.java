64aaa34089a4d1ee315610ea48ac4a4cc3dae460
shivanirathore24 <shivani.rathore2409@gmail.com>
2023-05-07 22:09:25 +0530

    Added: Queue in Collections

--- Queues/src/queue_collection/QueueCollections.java
+++ Queues/src/queue_collection/QueueCollections.java
@@ -11,10 +11,12 @@ public class QueueCollections {
 		queue.add(10);  //adding element
 		queue.add(20);
 		queue.add(30);   // in Queue : 10-->20-->30
-		System.out.println(queue.remove());  //throws exception "NoSuchElementException" if queue is empty
-		System.out.println(queue.poll());  // same as remove() but returns "null" if queue is empty
-		System.out.println(queue.peek());  // returns null if queue is empty
-		System.out.println(queue.element()); // same as peek() but returns throws exception "NoSuchElementException" if queue is empty
+		System.out.println(queue.size());       //present in Collection interface
+		System.out.println(queue.isEmpty());    //present in Collection interface
+		System.out.println(queue.remove());     //throws exception "NoSuchElementException" if queue is empty
+		System.out.println(queue.poll());       // same as remove() but returns "null" if queue is empty
+		System.out.println(queue.peek());       // returns null if queue is empty
+		System.out.println(queue.element());    // same as peek() but returns throws exception "NoSuchElementException" if queue is empty
 		
 	}
 
